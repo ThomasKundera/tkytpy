e2b2939e824ab08a3b107481d8fe14737b83dd41
TK <thomas.kundera@yahoo.fr>
2024-03-20 09:21:14 +0100

    Priority Queue, semaphore and tests

--- tkqueue.py
+++ tkqueue.py
@@ -3,23 +3,35 @@ import threading
 import queue
 import time
 import datetime
+from functools import total_ordering
 
 import tksingleton
 
+@total_ordering
 class TkTask:
-  def __init__(self,task):
+  def __init__(self,task,priority=0,semaphore=None):
+    self.semaphore=semaphore
+    self.priority=priority
     self.task=task
 
   def run(self):
     self.task()
+    if (self.semaphore):
+      self.semaphore.release()
+
+  def __lt__(self,o):
+    return self.priority < o.priority
+
+  def __eq__(self,o):
+    return self.priority ==  o.priority
 
   def __str__(self):
     return str(self.task)
 
 class TkTaskUniq(TkTask):
-  def __init__(self,tid,task):
+  def __init__(self,tid,task,priority=0,semaphore=None):
     self.tid=tid
-    self.task=task
+    super().__init__(task,priority,semaphore)
 
   def run(self):
     self.task()
@@ -32,7 +44,7 @@ class QueueWork(metaclass=tksingleton.SingletonMeta):
 
   def __init__(self):
     self.lastuse=datetime.datetime.now()
-    self.q=queue.Queue()
+    self.q=queue.PriorityQueue()
     threading.Thread(target=self.worker, daemon=True).start()
 
   def do_work(self,item):
@@ -74,3 +86,17 @@ class QueueWorkUniq(QueueWork):
     super().add(item)
 
 
+def test_task():
+  print("Test task")
+  time.sleep(1)
+
+# --------------------------------------------------------------------------
+def main():
+  t1=TkTaskUniq("toto",test_task)
+  q=QueueWorkUniq()
+  q.add(t1)
+  q.join()
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
