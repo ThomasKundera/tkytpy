b3f745f355f23cd7960ecf3d7ae80a8c92accfaa
TK <thomas.kundera@yahoo.fr>
2024-03-20 23:38:06 +0100

    Setting stuff for commentspinner

--- ytcommentsspinner.py
+++ ytcommentsspinner.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python3
-import threading
-from threading import Semaphore
-
-import tksingleton
-from ytqueue import YtQueue, YtTask
+from sqlrecord              import get_dbobject
 from ytcommentsworkerrecord import YTCommentsWorkerRecord
+from ytspinner              import YTSpinner
 
 
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-class YTCommentsWorker(YTCommentsWorkerRecord):
-  def __init__(self,yid):
-    super().__init__(yid)
+class YTCommentsSpinner(YTSpinner):
+  def __init__(self,field_storage):
+    self.cls=YTCommentsWorkerRecord
+    super().__init__(field_storage)
 
-  def run(self):
-    self.semaphore=Semaphore(1)
-    while  (self.infulldownload): # FIXME: actual condition is more complex
-      self.semaphore.acquire()
-      self.call_download_chunck()
-
-  def call_download_chunck(self):
-    task=YtTask('populate:'+self.yid,self.download_chunck)
-    YtQueue().add(task)
-
-def comments_monitor(yid):
-  cm=YTCommentsWorker(yid)
-  cm.run()
 
 # --------------------------------------------------------------------------
-# --------------------------------------------------------------------------
-class YTCommentsMonitor(metaclass=tksingleton.SingletonMeta):
-  def __init__(self):
-    self.threads={}
-    return
+def main():
+  from fieldstorage      import FieldStorage
+  field_storage = FieldStorage()
+  ycs=YTCommentsSpinner(field_storage)
+  yts.run()
+  from ytqueue import YtQueue
+  YtQueue().join()
 
-  def add(self,yid):
-    logging.debug("YTCommentsMonitor.add(): START")
-    if yid in self.threads: return
-    self.threads[yid]=threading.Thread(target=comments_monitor, args=(yid,))
-    self.threads[yid].start()
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
---
+++ ytcommentworkerrecord.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import datetime
+import json
+import sqlalchemy
+from sqlalchemy.orm import Session
+
+from ytqueue         import YtQueue, YtTask
+from sqlsingleton    import SqlSingleton, Base
+from sqlrecord       import SqlRecord, get_dbsession, get_dbobject
+
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+class YTCommentWorkerRecord(SqlRecord,Base):
+  __tablename__            = 'ytcommentworkerrecord0_1'
+  tid                      = sqlalchemy.Column(sqlalchemy.Unicode(50),primary_key=True)
+  lastwork                 = sqlalchemy.Column(sqlalchemy.DateTime)
+  etag                     = sqlalchemy.Column(sqlalchemy.Unicode(100))
+  nextcmtpagetoken         = sqlalchemy.Column(sqlalchemy.Unicode(200))
+
+  def __init__(self,dbsession,tid,commit=True):
+    self.tid=tid
+
+  def get_priority(self):
+    # FIXME: refreshing priority value is also complex
+    if not(self.lastwork):
+      return 0
+    Δt=datetime.datetime.now()-self.lastwork
+    # Fit that between 1000 and 2000
+    # FIXME
+    return 3600*24*365-Δt.total_seconds()
+
+  def populate_default(self):
+    self.lastwork=None
+    self.etag=None
+    self.nextcmtpagetoken=None
+
+  def populate(self,youtube):
+    logging.debug("YTCommentWorkerRecord.populate(): START")
+
+    logging.debug("YTCommentWorkerRecord.populate(): END")
+
+
+
+# --------------------------------------------------------------------------
+def main():
+  Base.metadata.create_all()
+  dbsession=SqlSingleton().mksession()
+  ycwd=dbsession.query(YTCommentWorkerRecord)
+  for ycw in ytwd:
+    ycw.call_populate()
+  YtQueue().join()
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
