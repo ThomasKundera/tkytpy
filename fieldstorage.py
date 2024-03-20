92ace5d8690176ebfc0e99895a68bee87ca37f95
TK <thomas.kundera@yahoo.fr>
2024-03-20 09:35:44 +0100

    get_dict() method (that should replace others, maybe), test working

--- fieldstorage.py
+++ fieldstorage.py
@@ -8,10 +8,40 @@ class FieldStorage:
     Base.metadata.create_all()
     return
 
+
+  def get_dict(self,cls):
+    dbsession=SqlSingleton().mksession()
+    d={}
+    for o in dbsession.query(cls):
+      k=o.get_id()
+      d[k]=o
+    return d
+
   def get_videos(self):
     dbsession=SqlSingleton().mksession()
-    vl={}
+    vd={}
     for v in dbsession.query(YTVideo):
-      v.resurect()
-      vl[v.yid]=v
+      vd[v.yid]=v
+    return vd
+
+  def get_theads(self):
+    dbsession=SqlSingleton().mksession()
+    td={}
+    for d in dbsession.query(YTVideo):
+      dl[v.yid]=v
     return vl
+
+
+
+
+# --------------------------------------------------------------------------
+def main():
+  from ytthreadrecord import YTThreadRecord
+  fs=FieldStorage()
+  vl=fs.get_dict(YTThreadRecord)
+  print(vl)
+
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
