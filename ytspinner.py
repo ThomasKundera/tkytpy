a8420b7c16adff29a1168dc42f6ef260e260ea2d
TK <thomas.kundera@yahoo.fr>
2024-03-20 10:21:29 +0100

    Theaded spin, test works

--- ytspinner.py
+++ ytspinner.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 import time
 import threading
-from threading import Semaphore
-
-import tksingleton
-from ytqueue import YtQueue, YtTaskSemaphore
+from threading import Thread, Semaphore
 
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
@@ -13,39 +10,46 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 # --------------------------------------------------------------------------
 class YTSpinner:
   def __init__(self,field_storage):
+    logging.debug("YTSpinner.__init__(): START")
     self.field_storage=field_storage
     self.semaphore=Semaphore(1)
-    self.spin()
+    self.spint = Thread(target=self.spin)
+    self.spint.start()
 
-  def call_populates(self,item):
+  def call_populate(self,item):
+    logging.debug("YTSpinner.call_populate: START")
     priority=item[0]
     t=item[1]
-    yid=t.get_id()
-    yid=t.get_id()
     self.semaphore.acquire()
-    task=YtTaskSemaphore('populate:'+type(t).__name__+"-"+yid,t.queued_populate,self.semaphore,priority)
-    YtQueue().add(task)
+    t.call_populate(priority,self.semaphore)
+    logging.debug("YTSpinner.call_populate: END")
 
   def queued_populate(self,youtube):
+    logging.debug("YTSpinner.queued_populate: START")
     time.sleep(10) # Just for tests
 
   def spin(self):
+    logging.debug("YTSpinner.spin: START")
     if (not self.field_storage):
       return # For tests, mostly
     while True:
       ol=[]
       od=self.field_storage.get_dict(self.cls)
       for o in od.values():
-        p=o.RefreshPriority()
+        p=o.get_priority()
         ol.append((p,o))
       ols=sorted(ol, key=lambda x: x[0])
-      self.call_populates(ols[0])
-
+      if (len(ols)):
+        self.call_populate(ols[0])
 
 # --------------------------------------------------------------------------
 def main():
-  ys=YTSpinner(None)
-  print(type(ys).__name__)
+  from fieldstorage      import FieldStorage
+  from sqltestrecord import TestRecord
+  field_storage = FieldStorage()
+  field_storage
+  ys=YTSpinner(field_storage,TestRecord)
+  ys.spint.join()
   return
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
