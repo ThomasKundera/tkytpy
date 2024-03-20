052d570af72b5e4ff6aa86a04d7ecece316b9203
TK <thomas.kundera@yahoo.fr>
2024-03-20 21:41:33 +0100

    Now using nexttreadpagetoken. Test works

--- ytthreadworkerrecord.py
+++ ytthreadworkerrecord.py
@@ -15,7 +15,7 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 # --------------------------------------------------------------------------
 # --------------------------------------------------------------------------
 class YTThreadWorkerRecord(SqlRecord,Base):
-  __tablename__ = 'ytthreadworkers0_1'
+  __tablename__ = 'ytthreadworkers0_2'
   yid                      = sqlalchemy.Column(sqlalchemy.Unicode(50),primary_key=True)
   firstthreadcidcandidate  = sqlalchemy.Column(sqlalchemy.Unicode(50))
   firstthreadcid           = sqlalchemy.Column(sqlalchemy.Unicode(50))
@@ -23,17 +23,17 @@ class YTThreadWorkerRecord(SqlRecord,Base):
   nextcmtpagetoken         = sqlalchemy.Column(sqlalchemy.Unicode(200))
   lastwork                 = sqlalchemy.Column(sqlalchemy.DateTime)
 
-
   def __init__(self,dbsession,yid,commit=True):
     self.yid=yid
     super().__init__(dbsession,commit)
-
 
   def get_priority(self):
     # FIXME: refreshing priority value is also complex
     if not(self.lastwork):
       return 0
     Δt=datetime.datetime.now()-self.lastwork
+    # Fit that between 1000 and 2000
+    # FIXME
     return 3600*24*365-Δt.total_seconds()
 
   def populate_default(self):
@@ -47,23 +47,33 @@ class YTThreadWorkerRecord(SqlRecord,Base):
     logging.debug("YTThreadWorkerRecord.populate(): START")
     dbsession=get_dbsession(self)
     if not (self.firstthreadcid):
-      logging.debug(type(self).__name__+".populate(): 1")
       if (not self.nexttreadpagetoken):
-        logging.debug(type(self).__name__+".populate(): 2")
-        request=youtube.commentThreads().list(part='snippet',videoId=self.yid,maxResults=100,textFormat='html')
-        logging.debug(type(self).__name__+".populate(): 3")
-        result=request.execute()
-        nbc=result['pageInfo']['totalResults']
-
-        for thread in result['items']:
-          tlc=thread['snippet']['topLevelComment']
-          cid=tlc['id']
-          if (not self.firstthreadcidcandidate):
-            self.firstthreadcidcandidate=cid
-          c=get_dbobject(YTCommentRecord,cid,dbsession)
-          c.fill_from_json(tlc,False)
-        if (nbc)<100: # we have all
-          self.firstthreadcid=self.firstthreadcidcandidate
+        request=youtube.commentThreads().list(
+          part='snippet',
+          videoId=self.yid,
+          maxResults=100,
+          textFormat='html')
+      else:
+        request=youtube.commentThreads().list(
+          part='snippet',
+          videoId=self.yid,
+          pageToken=self.nexttreadpagetoken,
+          maxResults=100,
+          textFormat='html')
+      result=request.execute()
+      nbc=result['pageInfo']['totalResults']
+      for thread in result['items']:
+        tlc=thread['snippet']['topLevelComment']
+        cid=tlc['id']
+        if (not self.firstthreadcidcandidate):
+          self.firstthreadcidcandidate=cid
+        c=get_dbobject(YTCommentRecord,cid,dbsession)
+        c.fill_from_json(tlc,False)
+      if (nbc)<100: # we have all
+        self.firstthreadcid=self.firstthreadcidcandidate
+      self.nexttreadpagetoken=result['nextPageToken']
+    else:
+      time.sleep(1) # FIXME
     self.lastwork=datetime.datetime.now()
     dbsession.commit()
     logging.debug("YTThreadWorkerRecord.populate(): END")
