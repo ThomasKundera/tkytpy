8355dbadca5ecffebdf612d3a2d4ea24394abe1c
TK <thomas.kundera@yahoo.fr>
2024-03-20 09:22:37 +0100

    Generic class for records

---
+++ sqlrecord.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import datetime
+import json
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import class_mapper
+from sqlsingleton import SqlSingleton, Base
+from ytqueue      import YtQueue, YtTask
+
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+# --------------------------------------------------------------------------
+def get_dbsession(o):
+  dbsession=Session.object_session(o)
+  if not dbsession:
+    dbsession=SqlSingleton().mksession()
+  return (dbsession)
+
+# --------------------------------------------------------------------------
+def get_dbobject(myclass,key,dbsession=None):
+  if not (dbsession):
+    dbsession=SqlSingleton().mksession()
+  o=dbsession.query(myclass).get(key)
+  if (not o):
+    o=myclass(dbsession,key)
+    dbsession.add(o)
+  return (o)
+
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+class SqlRecord:
+  def __init__(self,dbsession,commit=True):
+    dbsession.add(self)
+    self.populate_default()
+    if (commit):
+      dbsession.commit()
+
+  def get_id(self):
+    mapper = class_mapper(type(self))
+    return (getattr(self,mapper.primary_key[0].key))
+
+  def copy_from(self,o):
+    #r = inspect(self.myclass).columns
+    mapper = class_mapper(self)
+    prim = set([c.key for c in mapper.primary_key])
+    for k in [p.key for p in mapper.iterate_properties if p.key not in prim]:
+      value = getattr(o, k)
+      setattr(self, k, value)
+
+  def call_populate(self,priority=0,semaphore=None):
+    logging.debug(type(self).__name__+".populate(): START")
+    task=YtTask('populate:'+type(self).__name__+"-"+self.get_id(),self.queued_populate,priority,semaphore)
+    YtQueue().add(task)
+
+  def get_priority(self):
+    logging.debug(type(self).__name__+".get_priority(): START")
+    # Lower the more prioritized, number should roughlt reflects
+    # time (in second) before next update would be nice.
+    # Refresh priority should depends heavily on interractive accesses
+    # but we don't have that metric yet (FIXME)
+    return sys.maxsize
+
+  def __str__(self):
+    s=str(type(self).__name__)+" : { "
+    mapper = class_mapper(type(self))
+    for k in [p.key for p in mapper.iterate_properties]:
+      value = getattr(self, k)
+      s+=str(k)+" : "+str(value)+","
+    s+=" } "
+    return s
+
+  def queued_populate(self,youtube):
+    return
+
+  def populate_default(self):
+    return
+
+class TestRecord(SqlRecord,Base):
+  __tablename__ = 'test2'
+  name              = sqlalchemy.Column(sqlalchemy.Unicode(100),primary_key=True)
+  pp                = sqlalchemy.Column(sqlalchemy.Unicode(200))
+  pa                = sqlalchemy.Column(sqlalchemy.Unicode(200))
+  pe                = sqlalchemy.Column(sqlalchemy.Unicode(200))
+
+  def __init__(self,dbsession,name):
+    print("TestRecord: __init__("+name+")")
+    self.name=name
+    super().__init__(dbsession)
+
+
+# --------------------------------------------------------------------------
+def main():
+  Base.metadata.create_all()
+  tr1=get_dbobject(TestRecord,'toto')
+  print(tr1.get_id())
+  return
+  tr2=get_dbobject(TestRecord,'tutu')
+  tr3=get_dbobject(TestRecord,'titi')
+  tr4=get_dbobject(TestRecord,'tata')
+  dbsession=get_dbsession(tr1)
+  dbsession.commit()
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
+
