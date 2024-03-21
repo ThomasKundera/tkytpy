6469c951589a2411edde790386cefdf825d91d31
TK <thomas.kundera@yahoo.fr>
2024-03-21 15:47:40 +0100

    Fine

--- dataspinnermanager.py
+++ dataspinnermanager.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-#from ytvideosspinner          import YTVideosSpinner
+from ytvideosspinner          import YTVideosSpinner
 from ytthreadsspinner        import YTThreadsSpinner
 from ytcommentsspinner       import YTCommentsSpinner
 
 
 class DataSpinnersManager:
   def __init__(self,field_storage):
-    #self.yvs=YTVideosSpinner()
+    self.yvs=YTVideosSpinner  (field_storage)
     self.yts=YTThreadsSpinner (field_storage)
     self.ycs=YTCommentsSpinner(field_storage)
 
   def run(self):
-    #self.yvs.run()
+    self.yvs.run()
     self.yts.run()
     self.ycs.run()
 
@@ -20,6 +20,7 @@ def main():
   from fieldstorage      import FieldStorage
   field_storage = FieldStorage()
   dsm=DataSpinnersManager(field_storage)
+  dsm.run()
 
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
