29d251232216713c2da5b7f8484631cf48ed2dab
TK <thomas.kundera@yahoo.fr>
2024-03-21 15:40:39 +0100

    So simple and beautiful

--- ytvideosspinner.py
+++ ytvideosspinner.py
@@ -1,46 +1,27 @@
 #!/usr/bin/env python3
-import threading
-from threading import Semaphore
+from sqlrecord            import get_dbobject, get_dbsession
+from ytthreadworkerrecord import YTThreadWorkerRecord
+from ytspinner            import YTSpinner
+from ytvideorecord        import YTVideoRecord
 
-import tksingleton
-from ytqueue import YtQueue, YtTaskSemaphore
+from sqlsingleton import SqlSingleton, Base
 
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 # --------------------------------------------------------------------------
 # --------------------------------------------------------------------------
-class YTVideosSpinner:
+class YTVideosSpinner(YTSpinner):
   def __init__(self,field_storage):
-    self.field_storage=field_storage
-    self.semaphore=Semaphore(1)
-    self.spin()
-
-
-  def call_populates(self,item):
-    priority=item[0]
-    v=item[1]
-    self.semaphore.acquire()
-    task=YtTaskSemaphore('populate:'+v.yid,v.queued_populate,self.semaphore,priority)
-    YtQueue().add(task)
-
-  def spin(self):
-    while True:
-      vl=[]
-      vd=self.field_storage.get_videos()
-      for v in vd.values():
-        p=v.RefreshPriority()
-        vl.append((p,v))
-      vls=sorted(vl, key=lambda x: x[0])
-      v=vls[0][1]
-      self.call_populates(vls[0])
-
+    super().__init__(field_storage,YTVideoRecord)
 
 # --------------------------------------------------------------------------
 def main():
   from fieldstorage      import FieldStorage
   field_storage = FieldStorage()
   yvs=YTVideosSpinner(field_storage)
+  yvs.run()
+
 
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
