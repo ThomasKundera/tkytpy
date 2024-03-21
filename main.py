7243772cd8ba15e4643a70606e79972019d798a1
TK <thomas.kundera@yahoo.fr>
2024-03-21 15:47:48 +0100

    Works

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-from cmdnview          import CmdNView
-from filestorage       import FileStorage
-from fieldstorage      import FieldStorage
-from tkyt              import TkYt
-
+from filestorage        import FileStorage
+from fieldstorage       import FieldStorage
+from cmdnview           import CmdNView
+from tkyt               import TkYt
+from dataspinnermanager import DataSpinnersManager
 
 
 class Manager:
   def __init__(self):
     self.file_storage  = FileStorage ()
     self.field_storage = FieldStorage()
+    self.dsm           = DataSpinnersManager(self.field_storage)
     self.tkyt          = TkYt(self.field_storage)
     self.tkyt.setup()
     self.cmd_n_view    = CmdNView    (self.tkyt)
 
   def run(self):
+    self.dsm.run()
     self.cmd_n_view.run()
     return
 
