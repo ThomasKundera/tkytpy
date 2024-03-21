20406915642e1dfd70abd73c5ebc9e7c695d8abe
TK <thomas.kundera@yahoo.fr>
2024-03-21 11:18:46 +0100

    Allowed custom wait time (for tests), wait time is based on last call

--- ytqueue.py
+++ ytqueue.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+import datetime
 
 from tksecrets import google_api_key
 from googleapiclient.discovery import build
@@ -20,26 +21,32 @@ class YtTask(tkqueue.TkTaskUniq):
 # Thus, we can queue googleapi requests here,
 # without worrying of multitheads (hoppefully)
 class YtQueue(tkqueue.QueueWorkUniq):
-  def __init__(self):
+  def __init__(self,wait_time=10):
     self.youtube=build('youtube','v3',
                        developerKey=google_api_key, cache_discovery=False)
+    self.wait_time=wait_time
+    self.lastrun=datetime.datetime.now()
     super().__init__()
 
   def do_work(self,item):
-    time.sleep(10) # This impose less than 10.000 calls per day.
+    Δt=datetime.datetime.now()-self.lastrun
+    st=max(self.wait_time-Δt.total_seconds(),0)
+    logging.debug("YtQueue.do_work(): Sleeping "+str(st)+"s")
+    time.sleep(st) # This impose less than 10.000 calls per day.
     # FIXME: using etags in headers is a must.
     item.run(self.youtube)
     del self.taskdict[item.tid]
 
 class TestClass:
-  def __init__(self,yid):
-    self.yid=yid
+  def __init__(self,someid):
+    self.someid=someid
     time.sleep(1)
-    task=YtTask('populate:'+self.yid,self.populate)
-    YtQueue().add(task)
+    task=YtTask('populate:'+self.someid,self.populate)
+    YtQueue(1).add(task)
 
   def populate(self,youtube):
-    request=youtube.commentThreads().list(part='id',videoId=self.yid,maxResults=100,textFormat='html')
+    request=youtube.comments().list(part='id,snippet',parentId=self.someid,maxResults=2,textFormat='html')
+    #request=youtube.commentThreads().list(part='id,snippet',videoId=self.someid,maxResults=2,textFormat='html')
     print("\n-----------------\n")
     print(request.execute())
     print("\n-----------------\n")
@@ -47,7 +54,8 @@ class TestClass:
 # --------------------------------------------------------------------------
 def main():
   logging.debug("tkyoutube test: START")
-  t1=TestClass('j2GXgMIYgzU')
+  t1=TestClass('Ugw2LzdFdeWCnk6vwUV4AaABAg')
+  #t1=TestClass('j2GXgMIYgzU')
   #t1=TestClass('iphcyNWFD10')
   #t1=TestClass('aBr2kKAHN6M')
   #t1=TestClass('aBr2kKAHN6M')
