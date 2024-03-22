00e83792c2d3d612c5df3b07322fe71439e1345e
TK <thomas.kundera@yahoo.fr>
2024-03-22 13:44:28 +0100

    Do some stuff. Test works

--- tkyt.py
+++ tkyt.py
@@ -2,6 +2,7 @@
 import tksingleton
 
 from ytvideolist       import YTVideoList
+from ytcommentthread   import YTCommentThreadList
 
 class TkYt(metaclass=tksingleton.SingletonMeta):
   def __init__(self,field_storage):
@@ -9,20 +10,42 @@ class TkYt(metaclass=tksingleton.SingletonMeta):
     return
 
   def setup(self):
-    self.videos=YTVideoList(self.field_storage)
+    self.videos           =YTVideoList(self.field_storage)
+    self.commentthreadlist=YTCommentThreadList()
 
   def add_video(self,yid):
-    self.videos.add_from_yid(yid.strip())
+    yid=yid.strip()
+    if (len(yid)!=11):
+      yid=yid.split('?')[1].split('=')[1][0:11]
+    if (yid):
+      self.videos.add_from_yid(yid)
 
   def get_video_list(self):
     return self.videos.get_video_dict()
 
   def get_newest_thread_of_interest(self):
-    return {}
+    yth=self.commentthreadlist.get_newest_thread_of_interest()
+    return yth.to_dict()
 
   def get_oldest_thread_of_interest(self):
-    return {}
+    yth=self.commentthreadlist.get_oldest_thread_of_interest()
+    return yth.to_dict()
 
   def get_unseen_new_threads(self):
     return {}
 
+
+# --------------------------------------------------------------------------
+def main():
+  from fieldstorage      import FieldStorage
+  field_storage = FieldStorage()
+  tk=TkYt(field_storage)
+  tk.setup()
+  tk.add_video("https://www.youtube.com/watch?v=LaVip3J__8Y")
+  return
+  print(tk.get_oldest_thread_of_interest())
+  print(tk.get_newest_thread_of_interest())
+
+# --------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
