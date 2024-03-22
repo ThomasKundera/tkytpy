230f8d1935511cc3c713cb2141ebfca8d235d360
TK <thomas.kundera@yahoo.fr>
2024-03-22 11:53:14 +0100

    Added the ytthreadsevaluationthread

--- dataspinnermanager.py
+++ dataspinnermanager.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-from ytvideosspinner          import YTVideosSpinner
-from ytthreadsspinner        import YTThreadsSpinner
-from ytcommentsspinner       import YTCommentsSpinner
-
+from ytvideosspinner           import YTVideosSpinner
+from ytthreadsspinner          import YTThreadsSpinner
+from ytcommentsspinner         import YTCommentsSpinner
+from ytthreadsevaluationthread import YTThreadsEvaluationThread
 
 class DataSpinnersManager:
   def __init__(self,field_storage):
-    self.yvs=YTVideosSpinner  (field_storage)
-    self.yts=YTThreadsSpinner (field_storage)
-    self.ycs=YTCommentsSpinner(field_storage)
+    self.yvs=YTVideosSpinner           (field_storage)
+    self.yts=YTThreadsSpinner          (field_storage)
+    self.ycs=YTCommentsSpinner         (field_storage)
+    self.yet=YTThreadsEvaluationThread (field_storage)
 
   def run(self):
     self.yvs.run()
     self.yts.run()
     self.ycs.run()
+    self.yet.run()
 
 # --------------------------------------------------------------------------
 def main():
