70feffce615a0e4d105b406b564a7c35778a11e3
TK <thomas.kundera@yahoo.fr>
2024-03-22 11:52:39 +0100

    Counting number of threads processed: sould always be 100

--- ytthreadsevaluationthread.py
+++ ytthreadsevaluationthread.py
@@ -23,32 +23,18 @@ class YTThreadsEvaluationThread:
     self.spint.start()
     logging.debug("YTThreadsEvaluationThread.run: END")
 
-  def do_spin(self):
-    logging.debug("YTThreadsEvaluationThread.do_spin(): START")
-    ol=[]
-
-    for o in od.values():
-      p=o.get_priority()
-      ol.append((p,o))
-    ols=sorted(ol, key=lambda x: x[0])
-    if (len(ols) and ols[0][0] < 1000000): # FIXME
-      logging.debug("YTThreadsEvaluationThread.do_spin(): selected "+str(ols[0]))
-      self.call_populate(ols[0])
-    else:
-      logging.debug("YTThreadsEvaluationThread.do_spin(): Nothing to do")
-      time.sleep(1)
-    logging.debug("YTThreadsEvaluationThread.do_spin(): END")
-
-
   def do_spin(self,chunck_size):
     tteval=self.dbsession.query(YTCommentWorkerRecord).filter(YTCommentWorkerRecord.lastcompute==None).limit(chunck_size) # FIXME: We could only get tid, here.
     # FIXME: as at many other places this will only work once.
     # Updates are not handled
+    count=0
     for t in tteval:
       #time.sleep(0.1) # FIXME This is to let time to other threads as there is
       #                # no thread priority available
       YTCommentThread(t.tid,self.dbsession).set_interest(False)
+      count+=1
     self.dbsession.commit()
+    logging.debug("YTThreadsEvaluationThread.do_spin: END: processed "+str(count)+" items")
 
   def spin(self):
     logging.debug("YTThreadsEvaluationThread.spin: START")
