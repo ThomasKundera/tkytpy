b41eb2c1e858967352cbc5ca39afa51fe0f4d526
TK <thomas.kundera@yahoo.fr>
2024-04-16 13:43:53 +0200

    Not sure it's OK, but...

--- ytthreadsevaluationthread.py
+++ ytthreadsevaluationthread.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import time
 from threading import Thread, Semaphore
+#from sqlalchemy import nulls_first
+from sqlalchemy import case, or_
 
 from ytcommentworkerrecord  import YTCommentWorkerRecord
 from ytcommentthread        import YTCommentThread
@@ -25,11 +27,18 @@ class YTThreadsEvaluationThread:
     logging.debug("YTThreadsEvaluationThread.run: END")
 
   def do_spin(self,chunck_size):
-    tteval=self.dbsession.query(YTCommentWorkerRecord).order_by(YTCommentWorkerRecord.lastwork.desc()).limit(chunck_size) # FIXME: We could only get tid, here.
+    chunck_size=10 # DEBUG
+    #tteval=self.dbsession.query(YTCommentWorkerRecord).filter(YTCommentWorkerRecord.done==True).order_by(nulls_first(YTCommentWorkerRecord.lastcompute)).limit(chunck_size)
+    #tteval=self.dbsession.query(YTCommentWorkerRecord).filter(YTCommentWorkerRecord.done==True).order_by(
+    #  case([(or_(YTCommentWorkerRecord.lastcompute.is_(None), YTCommentWorkerRecord.lastcompute == ""), 0)], else_=1),YTCommentWorkerRecord.lastcompute ).limit(chunck_size)
+    tteval=self.dbsession.query(YTCommentWorkerRecord).filter(YTCommentWorkerRecord.done==True).order_by(YTCommentWorkerRecord.lastcompute).limit(chunck_size)
+
     #tteval=self.dbsession.query(YTCommentWorkerRecord).filter(YTCommentWorkerRecord.interest_level!=0).order_by(YTCommentWorkerRecord.interest_level.desc()).limit(chunck_size)
 
     count=0
     for t in tteval:
+      time.sleep(0.1) # FIXME This is to let time to other threads as there is
+      #                # no thread priority available
       YTCommentThread(t.tid,self.dbsession).set_interest()
       count+=1
     self.dbsession.commit()
