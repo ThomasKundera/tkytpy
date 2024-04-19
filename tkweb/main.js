65454d2e7aa7ac8ee9591f1c572b3f2e13ca63f2
TK <thomas.kundera@yahoo.fr>
2024-04-19 08:51:26 +0200

    Oldest thread (in progress)

--- tkweb/main.js
+++ tkweb/main.js
@@ -1,24 +1,49 @@
-async function update_main() {
-  try {
-    const response = await fetch("http://localhost:8000/post", {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: ('{ "command": "get_main_stuff" }')
-    });
-    const yidtext = await response.text();
-    const yidjson=JSON.parse(yidtext);
-    // populateVideoList(yidjson);
+function populate_oldest_thread(obj) {
+  const oldest_thread_div = document.querySelector("#oldestthreaddiv");
+  otdiv.innerHTML = "";
+  const tlc = obj.tlc;
 
-  } catch (e) {
-    console.error(e);
+  const mvldiv = document.createElement("div");
+  mvldiv.setAttribute('class','videolist');
+  for (const ytv of ytvlist) {
+    const mdv = document.createElement("div");
+    mdv.setAttribute('class','video');
+
+    const ma1 = document.createElement("a");
+    ma1.setAttribute('href',"https://www.youtube.com/watch?v="+ytv.yid)
+    const mi=document.createElement("img");
+    mi.setAttribute('src',ytv.thumb_url_s);
+    mi.setAttribute('width',"120");
+    mi.setAttribute('height',"90");
+    ma1.appendChild(mi);
+    mdv.appendChild(ma1);
+
+    const ma2 = document.createElement("a");
+    ma2.textContent = ytv.title;
+    ma2.setAttribute('href',"/video.html?yid="+ytv.yid)
+    mdv.appendChild(ma2)
+
+    const mdc = document.createElement("div");
+    mdc.setAttribute('class','vcontrol');
+
+    const mip = document.createElement("input");
+    mip.setAttribute('type','checkbox');
+    mip.setAttribute('id',"vmonitor");
+    mip.setAttribute('onclick',"monitor_video()");
+
+    mdc.appendChild(mip);
+    mdv.appendChild(mdc)
+
+    mvldiv.appendChild(mdv)
   }
+  videodiv.appendChild(mvldiv);
 }
 
 
-async function get_oldest_thread_of_interest() {
+
+
+
+async function update_oldest_thread_of_interest() {
   try {
     const response = await fetch("http://localhost:8000/post", {
       method: 'POST',
@@ -28,8 +53,9 @@ async function get_oldest_thread_of_interest() {
       },
       body: ('{ "command": "get_oldest_thread_of_interest" }')
     });
-    const yidtext = await response.text();
-    const yidjson=JSON.parse(yidtext);
+    const cidtext = await response.text();
+    const cidjson=JSON.parse(cidtext);
+    populate_oldest_thread(cidjson);
   } catch (e) {
     console.error(e);
   }
@@ -38,5 +64,5 @@ async function get_oldest_thread_of_interest() {
 
 // On document load
 window.addEventListener("load", function() {
-  update_main();
+  update_oldest_thread_of_interest();
 });
