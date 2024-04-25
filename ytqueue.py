0d4909c50878a31a8d12bf4b65669c6463798b61
TK <thomas.kundera@yahoo.fr>
2024-04-25 13:22:28 +0200

    Uses YtApi, and works

--- ytqueue.py
+++ ytqueue.py
@@ -2,11 +2,9 @@
 import time
 import datetime
 
-from tksecrets import google_api_key
-from googleapiclient.discovery import build
-
 from tkqueue import QueueWorkUniq
 from sqltask import SqlTaskUniq
+from ytapi import YtApi
 
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
@@ -16,9 +14,11 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 # fetching the data?
 class YtTask(SqlTaskUniq):
   def do_run(self,youtube):
+    logging.debug(type(self).__name__+".do_run(): START")
     self.o.sql_task_threaded(self.dbsession,youtube)
 
   def run(self,youtube):
+    logging.debug(type(self).__name__+".run(): START")
     self.pre_run()
     self.do_run(youtube)
     self.post_run()
@@ -29,47 +29,25 @@ class YtTask(SqlTaskUniq):
 # without worrying of multitheads (hoppefully)
 class YtQueue(QueueWorkUniq):
   def __init__(self,wait_time=10):
-    self.youtube=build('youtube','v3',
-                       developerKey=google_api_key, cache_discovery=False)
-    self.wait_time=wait_time
-    self.lastrun=datetime.datetime.now()
+    self.youtube=YtApi()
     super().__init__()
 
   def do_work(self,item):
-    now=datetime.datetime.now()
-    Δt=now-self.lastrun
-    self.lastrun=now
-    st=max(self.wait_time-Δt.total_seconds(),0)
-    logging.debug("YtQueue.do_work(): Sleeping "+str(st)+"s")
-    time.sleep(st) # This impose less than 10.000 calls per day.
-    # FIXME: using etags in headers is a must.
     item.run(self.youtube)
     del self.taskdict[item.tid]
-
-class TestClass:
-  def __init__(self,someid):
-    self.someid=someid
-    time.sleep(1)
-    task=YtTask('populate:'+self.someid,self.populate)
-    YtQueue(1).add(task)
-
-  def populate(self,youtube):
-    #request=youtube.comments().list(part='id,snippet',parentId=self.someid,maxResults=2,textFormat='html')
-    request=youtube.commentThreads().list(part='id,snippet',videoId=self.someid,maxResults=2,textFormat='html')
-    print("\n-----------------\n")
-    print(request.execute())
-    print("\n-----------------\n")
 
 # --------------------------------------------------------------------------
 def main():
   logging.debug("ytqueue test: START")
-  #t1=TestClass('Ugw2LzdFdeWCnk6vwUV4AaABAg')
-  t1=TestClass('j2GXgMIYgzU')
-  #t1=TestClass('iphcyNWFD10')
-  #t1=TestClass('aBr2kKAHN6M')
-  #t1=TestClass('aBr2kKAHN6M')
-
+  from sqltestrecord import TestRecord
+  from sqlsingleton import SqlSingleton, Base
+  Base.metadata.create_all()
+  dbsession=SqlSingleton().mksession()
+  t=TestRecord(dbsession,1)
+  t.call_sql_task_threaded()
   YtQueue().join()
+  time.sleep(2)
+  print(YtQueue().q.qsize())
   logging.debug("ytqueue test: END")
 
 # --------------------------------------------------------------------------
