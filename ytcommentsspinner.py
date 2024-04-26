2bf18457d6e009443098a879f166e200dd632fcd
TK <thomas.kundera@yahoo.fr>
2024-04-26 18:14:38 +0200

    Way better selection function

--- ytcommentsspinner.py
+++ ytcommentsspinner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from sqlalchemy import or_, and_
+import datetime
+from sqlalchemy import or_, and_, case
+from sqlalchemy.sql import func
 from ytcommentworkerrecord  import YTCommentWorkerRecord
 from ytspinner              import YTSpinner
 from sqlsingleton           import SqlSingleton
@@ -14,17 +16,35 @@ class YTCommentsSpinner(YTSpinner):
 
   def get_items_to_process(self):
     logging.debug(type(self).__name__+"get_items_to_process.(): START")
+    #dt=datetime.datetime(2024, 4, 26, 0, 31, 38).timestamp()
+    #sqlv=20240426003138
+    #print(dt)
+    #sys.exit(0)
     chunck_size=200 # FIXME
+    now=datetime.datetime.now().timestamp()
+    #print(datetime.datetime.now().timestamp())
+    #ycwr=self.dbsession.query(YTCommentWorkerRecord.lastwork,
+    #                          func.unix_timestamp(YTCommentWorkerRecord.lastwork),
+    #                          #func.timestampdiff('SECOND',func.now(),YTCommentWorkerRecord.lastwork),
+    #                          now-func.unix_timestamp(YTCommentWorkerRecord.lastwork)
+    #                          ).limit(chunck_size)
+    #for o in ycwr:
+    #  print(o)
+    #sys.exit(0)
     ycwr=self.dbsession.query(YTCommentWorkerRecord).join(
       YTVideoRecord,YTVideoRecord.yid==YTCommentWorkerRecord.yid).filter(
         and_(YTCommentWorkerRecord.done==False,
              YTVideoRecord.valid == True,
-             YTVideoRecord.suspended ==False)
-        ).order_by(YTCommentWorkerRecord.lastwork).limit(chunck_size)
+             YTVideoRecord.suspended ==False,
+             YTVideoRecord.monitor>0)
+        ).order_by( case((YTCommentWorkerRecord.lastwork == None,24./YTVideoRecord.monitor),
+                        else_=(now-func.unix_timestamp(YTCommentWorkerRecord.lastwork))/(3600*YTVideoRecord.monitor))).limit(chunck_size)
     d={}
     for o in ycwr:
+      #print (str(o.lastwork)+" "+str(o.yid))
       k=o.tid
       d[k]=o
+    #sys.exit(0)
     return d
 
 # --------------------------------------------------------------------------
