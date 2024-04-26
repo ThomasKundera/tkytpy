0463c2bd3fcf73d546c12074bf59f021bf10215d
TK <thomas.kundera@yahoo.fr>
2024-04-26 12:30:47 +0200

    Some enhancements

--- ytthreadsevaluationthread.py
+++ ytthreadsevaluationthread.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+import datetime
 from threading import Thread, Semaphore
 #from sqlalchemy import nulls_first
 from sqlalchemy import case, or_
@@ -28,12 +29,16 @@ class YTThreadsEvaluationThread:
     chunck_size=100
     tteval=dbsession.query(YTCommentWorkerRecord).filter(YTCommentWorkerRecord.done==True).order_by(YTCommentWorkerRecord.lastcompute).limit(chunck_size)
     count=0
+    Δt=datetime.datetime.now()-tteval[0].lastcompute
+    if (Δt.total_seconds() < 10*60):
+      # Useless to recompute so often, waiting a bit and leaving
+      time.sleep(600)
+      return
     for t in tteval:
-      print(t.lastcompute)
+      logging.debug("YTThreadsEvaluationThread.do_spin(): lastcompute: "+str(t.lastcompute))
       YTCommentThread(t.tid,dbsession).set_interest()
       count+=1
     dbsession.commit()
-    logging.debug("YTThreadsEvaluationThread.do_spin: END: processed "+str(count)+" items")
 
   def spin(self):
     logging.debug("YTThreadsEvaluationThread.spin: START")
