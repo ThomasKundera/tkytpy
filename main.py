32328caa824fae1aa380504b61fb5858d9386564
TK <thomas.kundera@yahoo.fr>
2024-04-27 12:37:23 +0200

    To please lint

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+"""Main entry point to tkyt Youtube Comment manager.
+This script runs both UI (as http interface) and data spinners.
+"""
+
 from filestorage        import FileStorage
 from fieldstorage       import FieldStorage
 from cmdnview           import CmdNView
@@ -7,7 +12,13 @@ from dataspinnermanager import DataSpinnersManager
 
 
 class Manager:
+  """
+  Manage and run all subclasses
+  """
   def __init__(self):
+    """
+    Initialize inteface and data instance and connect them.
+    """
     self.file_storage  = FileStorage ()
     self.field_storage = FieldStorage()
     self.dsm           = DataSpinnersManager(self.field_storage)
@@ -16,12 +27,15 @@ class Manager:
     self.cmd_n_view    = CmdNView    (self.tkyt)
 
   def run(self):
+    """
+    Run inteface and data instance, as separated threads
+    """
     self.dsm.run()
     self.cmd_n_view.run()
-    return
 
 # --------------------------------------------------------------------------
 def main():
+  """Starts all"""
   manager=Manager()
   manager.run()
 
@@ -29,4 +43,3 @@ def main():
 # --------------------------------------------------------------------------
 if __name__ == '__main__':
   main()
-        
