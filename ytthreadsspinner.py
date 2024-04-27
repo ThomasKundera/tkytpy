b646d1ecb1c4e00671ae422303c605b526568a4c
TK <thomas.kundera@yahoo.fr>
2024-04-27 09:13:28 +0200

    SQL slect item to process

--- ytthreadsspinner.py
+++ ytthreadsspinner.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+import time
+import datetime
+from sqlalchemy import or_, and_, case
+from sqlalchemy.sql import func
 from ytthreadworkerrecord import YTThreadWorkerRecord
 from ytspinner            import YTSpinner
+from ytvideorecord        import YTVideoRecord
 
 from sqlsingleton import SqlSingleton, Base, get_dbobject, get_dbsession
 
@@ -23,8 +28,42 @@ class YTThreadsSpinner(YTSpinner):
 
   def do_spin(self):
     self.update_from_videos()
-    super().do_spin()
+    super().do_spin_new()
 
+  def get_item_to_process(self):
+    logging.debug(type(self).__name__+"get_item_to_process.(): START")
+    now=datetime.datetime.now().timestamp()
+    # if Δt=now-lastwork
+    # Δt between 0 and 10⁸, about.
+    # log10(Δt) between 1 and 9, about
+    # min(log10(Δt),9) between 1 and 9
+    # 10-min(log10(Δt),9) between 1 and 10
+    # None should be 1.
+    # Then divide by monitor
+    # For monitor=1 None: 1 : wins against any Δt
+    # For monitor=2 None: 1 : wins against Δt<10⁸
+    # For monitor=3 None: 1 : wins against Δt<10⁷
+    # ...
+    # For monitor=10 None: 1 : always loose
+    ycwr=self.dbsession.query(YTThreadWorkerRecord).join(
+      YTVideoRecord,YTVideoRecord.yid==YTThreadWorkerRecord.yid).filter(
+        and_(YTVideoRecord.valid == True,
+             YTVideoRecord.suspended ==False,
+             YTVideoRecord.monitor>0)
+        ).order_by(
+          case(
+            (YTThreadWorkerRecord.lastwork == None,1000./YTVideoRecord.monitor),
+          else_=(
+            case(
+              (and_(
+              YTThreadWorkerRecord.firstthreadcid != None,
+              func.unix_timestamp(YTThreadWorkerRecord.lastwork)<300),11*1000./YTVideoRecord.monitor),
+              else_=1000*(10.-func.least(func.log10(now-func.unix_timestamp(YTThreadWorkerRecord.lastwork)),9))/YTVideoRecord.monitor)
+              )
+          )).limit(1)
+    for o in ycwr:
+      return (1000,o) # FIXME 1000 is arbirary
+    return None # No matching item found
 
 # --------------------------------------------------------------------------
 def main():
