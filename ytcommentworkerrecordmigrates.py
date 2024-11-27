6ea390e26e83fa9318872300939310d2a52c65ba
TK <thomas.kundera@yahoo.fr>
2024-11-27 14:02:26 +0100

    Simplified, for later

--- ytcommentworkerrecordmigrates.py
+++ ytcommentworkerrecordmigrates.py
@@ -9,21 +9,24 @@ from ytcommentworkerrecord import YTCommentWorkerRecord
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-class YTCommentWorkerRecord6(SqlRecord,Base):
-  __tablename__            = 'ytcommentworkerrecord0_6'
+class YTCommentWorkerRecordold(SqlRecord,Base):
+  __tablename__            = 'ytcommentworkerrecord0_8'
   tid                      = sqlalchemy.Column(sqlalchemy.Unicode(50),primary_key=True)
   yid                      = sqlalchemy.Column(sqlalchemy.Unicode(50))
   lastwork                 = sqlalchemy.Column(sqlalchemy.DateTime)
   done                     = sqlalchemy.Column(sqlalchemy.Boolean)
   interest_level           = sqlalchemy.Column(sqlalchemy.Integer)
+  most_recent_me           = sqlalchemy.Column(sqlalchemy.DateTime)
+  most_recent_reply        = sqlalchemy.Column(sqlalchemy.DateTime)
+  ignore_before            = sqlalchemy.Column(sqlalchemy.DateTime)
   lastcompute              = sqlalchemy.Column(sqlalchemy.DateTime)
   etag                     = sqlalchemy.Column(sqlalchemy.Unicode(100))
   nextcmtpagetoken         = sqlalchemy.Column(sqlalchemy.Unicode(200))
+  
 
   def __init__(self,dbsession,cid,commit=True):
     self.cid=cid
     super().__init__(dbsession,commit)
-
 
 
 def init_db():
@@ -32,12 +35,19 @@ def init_db():
 
 
 def migrates(dbsession):
+  """
+  Migrates all records in the old ytcommentworkerrecord table to the new
+  format.
+
+  This function should be called once, then removed from the codebase.
+  """
+  return -1
   n=0
-  for c6 in dbsession.query(YTCommentWorkerRecord6):
-    c7=get_dbobject(YTCommentWorkerRecord,c6.tid,dbsession,False)
-    dbsession.add(c7)
-    c7.copy_from(c6)
-    c7.lastcompute=None
+  for cold in dbsession.query(YTCommentWorkerRecordold):
+    cnew=get_dbobject(YTCommentWorkerRecord,cold.tid,dbsession,False)
+    dbsession.add(cnew)
+    cnew.copy_from(cold)
+    #cnew.lastcompute=None
     n+=1
     if (not n % 100):
       print (" ================== ROW "+str(n)+" PROCESSED ============")
